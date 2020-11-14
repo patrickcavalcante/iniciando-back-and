a42d1427cf613d95fd3c675c8b738ef2c0cde9bc
Patrick Cavalcante <patrickc.lima2@gmail.com>
2020-11-14 09:49:07 -0300

    Conectando Banco de Dados

--- src/database/migrations/1605055609211-CreateAppointments.ts
+++ src/database/migrations/1605055609211-CreateAppointments.ts
@@ -12,6 +12,7 @@ export class CreateAppointments1605055609211 implements MigrationInterface {
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import {startOfHour} from 'date-fns'
+import {getCustomRepository} from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -9,17 +10,11 @@ interface Request {
 }
 
 class CreateAppointmentService {
-
-    private appointmentsRepository : AppointmentsRepository;
-
-    constructor(appointmentsRepository: AppointmentsRepository){
-        this.appointmentsRepository = appointmentsRepository;
-    }
-
-    public execute({provider , date}: Request): Appointment {
+    public async execute({provider , date}: Request): Promise<Appointment>{
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository)
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(
             appointmentDate,
         );
 
@@ -27,10 +22,12 @@ class CreateAppointmentService {
             throw Error('This appointment is already booked');
         }
 
-        const appointment = this.appointmentsRepository.create({
+        const appointment = appointmentsRepository.create({
             provider,
             date:appointmentDate,
         });
+
+        await appointmentsRepository.save(appointment)
 
         return appointment;
     }
